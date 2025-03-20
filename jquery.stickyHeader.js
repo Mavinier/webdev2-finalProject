c77d54a99d785b55bb772231f0e221b1ceb03dc6
Mavinier <mavinier2010@gmail.com>
2025-03-20 14:18:00 -0300

    z-index

--- jquery.stickyHeader.js
+++ jquery.stickyHeader.js
@@ -1,7 +1,6 @@
 (function ($) {
     $.fn.stickyHeader = function () {
         var $element = this;
-        console.log("aaa")
         $(window).scroll(function () {
             $element.toggleClass('fixed', $(window).scrollTop() > 50);
         });
